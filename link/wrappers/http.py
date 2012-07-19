1400479dfdfc4fbc53c5df4b503e25d60e750874
david himrod <davidhimrod@davids-MacBook-Air.local>
2012-07-19 04:00:21 -0400

    making a good decorator for API requests

--- link/wrappers/http.py
+++ link/wrappers/http.py
@@ -1,8 +1,10 @@
 from link import Wrapper
 import json
 
-class Response(object):
+class Response(Wrapper):
     """
+    Allows you to decorate a function that is a response so that it has a proper 
+    format
     An API style response 
     """
     def __init__(self, response=None, warnings = None,
@@ -14,6 +16,24 @@ class Response(object):
            self.warnings.append(warnings)
 
         self.error = error
+        super(Response, self).__init__(wrap_name)
+
+    def __call__(self, func):
+        """
+        Return a function because this is a decorator
+        """
+        def respond(*kargs, **kwargs):
+            """
+            Gives itself as a parameter so that you can set the return value
+            """
+            try:
+                func(*kargs, response = self,  **kwargs)
+            except Exception as e:
+                self.error = "ERROR - %s " % e.message
+
+            return self
+
+        return respond
 
     def add_warning(self, msg, key = 'WARNING'):
         """
@@ -35,15 +55,14 @@ class Response(object):
         response = {'response':self.response}
 
         if self.warnings:
-            response['warnings'] = warnings
+            response['warnings'] = self.warnings
  
         if self.error:
-            response['error'] = error
+            response['error'] = self.error
 
         return response
-
-    @property
-    def json(self):
+    
+    def __str__(self):
         """
         Make the response into a string
         """
