6fd4a0492afbd064b4295cf98d8cded757621678
Dave Himrod <dhimrod@appnexus.com>
2014-04-15 17:04:14 +0000

    adding hive wrapper

--- link/wrappers/__init__.py
+++ link/wrappers/__init__.py
@@ -1,7 +1,11 @@
+"""
+I don't exactly love that you have to do this.  I will look for a new design
+"""
 from apiwrappers import *
 from dbwrappers import *
 from nosqlwrappers import *
 from consolewrappers import *
 from atlassianwrappers import *
 from alexawrappers import *
+from hivewrappers import *
 from elasticsearchwrappers import *
